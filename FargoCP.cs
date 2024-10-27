22c8e811ba3346adbe17571808540521d5438790
Henceforth <3412615165@qq.com>
2024-10-27 19:52:14 +0800

    文本

--- FargoCP.cs
+++ FargoCP.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace FargoCP
@@ -10,6 +11,23 @@ namespace FargoCP
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class FargoCP : Mod
 	{
-
+		public static Type gmod = null;
 	}
+
+	public class World : ModPlayer
+	{
+        public override void OnEnterWorld()
+        {
+			if(FargoCP.gmod != null)
+			{
+                Main.NewText(FargoCP.gmod.Name);
+			}
+			else
+			{
+				Main.NewText("¿ÕµÄ");
+			}
+			
+            base.OnEnterWorld();
+        }
+    }
 }
