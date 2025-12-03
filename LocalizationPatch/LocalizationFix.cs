89e4d5d9a9be2a1f3734a699fbbb5547c019faa5
De1tA5 <179575280+De1tA5@users.noreply.github.com>
2025-12-03 22:09:50 +0800

    代码更新

--- LocalizationPatch/LocalizationFix.cs
+++ LocalizationPatch/LocalizationFix.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
@@ -11,6 +12,14 @@ namespace FargoCP.LocalizationPatch
 {
     public class LocalizationFix:GlobalItem
     {
+        private readonly List<string> CalEnchant = [
+            "AstralEnchant","AtaxiaEnchant","AuricEnchant","BloodflareEnchant","DaedalusEnchant",
+            "EmpyreanEnchant","FathomSwarmerEnchant","FearmongerEnchant","GemTechEnchant","GodSlayerEnchant",
+            "OmegaBlueEnchant","PlaguebringerEnchant","PlagueReaperEnchant","SilvaEnchant","StatigelEnchant",
+            "SulphurousEnchant","TarragonEnchant"];
+
+        private readonly List<string> CalForce = ["AnnihilationForce", "DesolationForce", "DevastationForce", "ExaltationForce"];
+
         public override bool InstancePerEntity => true;
 
         public override bool IsLoadingEnabled(Mod mod)
@@ -21,32 +30,36 @@ namespace FargoCP.LocalizationPatch
         public TooltipLine TooltipsPatch(string text, string modName)
         {
             var tooltip = new TooltipLine(Mod, "TooltipPatch", "");
-            switch (modName)
+            var rounite = "Mods.FargoCP.FargosSoulsModDLCRecreated.ItemTooltips.";
+
+            if (modName == "cal" && Language.Exists(rounite + "Calamity." + text))
             {
-                case "cal":
-                    tooltip.Text = Language.GetTextValue("Mods.FargoCP.FargosSoulsModDLCRecreated.ItemTooltips.Calamity." + text);
-                    return tooltip;
-                case "thor":
-                    tooltip.Text = Language.GetTextValue("Mods.FargoCP.FargosSoulsModDLCRecreated.ItemTooltips.Thorium." + text);
-                    return tooltip;
-                case "spi":
-                    tooltip.Text = Language.GetTextValue("Mods.FargoCP.FargosSoulsModDLCRecreated.ItemTooltips.Spirit." + text);
-                    return tooltip;
-                case "noFargoCross":
-                    tooltip.Text = Language.GetTextValue("Mods.FargoCP.FargosSoulsModDLCRecreated.ItemTooltips.FargoSouls." + text);
-                    return tooltip;
-                default:
-                    return null;
+                tooltip.Text = Language.GetTextValue(rounite + "Calamity." + text);
+                return tooltip;
             }
+            else if (modName == "thor" && Language.Exists(rounite + "Thorium." + text))
+            {
+                tooltip.Text = Language.GetTextValue(rounite + "Thorium." + text);
+                return tooltip;
+            }
+            else if (modName == "spi" && Language.Exists(rounite + "Spirit." + text))
+            {
+                tooltip.Text = Language.GetTextValue(rounite + "Spirit." + text);
+                return tooltip;
+            }
+            else if (modName == "noFargoCross" && Language.Exists(rounite + "FargoSouls." + text))
+            {
+                tooltip.Text = Language.GetTextValue(rounite + "FargoSouls." + text);
+                return tooltip;
+            }
+            else return null;
         }
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
         {
             //原版物品跳出判断，避免栈溢出
-            if (item.ModItem == null) 
-            {
-                return;
-            }
+            if (item.ModItem == null) return;
+
 
             //删除原模组的文本
             if (item.ModItem.Mod.Name == "FargosSoulsModDLCRecreated" && item.ModItem !=null)
@@ -62,303 +75,51 @@ namespace FargoCP.LocalizationPatch
 
             #region 灾厄
 
-            
+            //遍历所有魔石 和 力
             if (Utility.FSDLC.Load && Utility.Cal.Load)
             {
                 string cal = "cal";
-                
-                #region 魔石
-
-                //星幻魔石
-                if (item.type == Utility.FSDLC.Item("AstralEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("AstralEnchant" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("AstralEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("AstralEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //渊泉魔石
-                if (item.type == Utility.FSDLC.Item("AtaxiaEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("AtaxiaEnchant" + ".Main", cal));
-                    if (Utility.Calbard.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("AtaxiaEnchant" + ".Calbard", cal));
-                    }
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("AtaxiaEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("AtaxiaEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //圣金源魔石
-                if (item.type == Utility.FSDLC.Item("AuricEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("AuricEnchant" + ".Main", cal));
-                    if (Utility.Calbard.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("AuricEnchant" + ".Calbard", cal));
-                    }
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("AuricEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("AuricEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //血炎魔石
-                if (item.type == Utility.FSDLC.Item("BloodflareEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("BloodflareEnchant" + ".Main", cal));
-                    if (Utility.Calbard.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("BloodflareEnchant" + ".Calbard", cal));
-                    }
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("BloodflareEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("BloodflareEnchant" + ".Tail", cal));
-                    }
-
-                }
-
-                //代达罗斯魔石
-                if (item.type == Utility.FSDLC.Item("DaedalusEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("DaedalusEnchant" + ".Main", cal));
-                    if (Utility.Calbard.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("DaedalusEnchant" + ".Calbard", cal));
-                    }
-                    tooltips.Add(TooltipsPatch("DaedalusEnchant" + ".Tail", cal));
-                }
-
-
-                //皇天魔石
-                if (item.type == Utility.FSDLC.Item("EmpyreanEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("EmpyreanEnchant" + ".Main", cal));
-                    tooltips.Add(TooltipsPatch("EmpyreanEnchant" + ".Tail", cal));
-                }
-
-                //幻海鱼群魔石
-                if (item.type == Utility.FSDLC.Item("FathomSwarmerEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("FathomSwarmerEnchant" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("FathomSwarmerEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("FathomSwarmerEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //神惧者魔石
-                if (item.type == Utility.FSDLC.Item("FearmongerEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("FearmongerEnchant" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("FearmongerEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("FearmongerEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //天钻魔石
-                if (item.type == Utility.FSDLC.Item("GemTechEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("GemTechEnchant" + ".Main", cal));
-                }
-
-                //弑神者魔石
-                if (item.type == Utility.FSDLC.Item("GodSlayerEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("GodSlayerEnchant" + ".Main", cal));
-                    if (Utility.Calbard.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("GodSlayerEnchant" + ".Calbard", cal));
-                    }
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("GodSlayerEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("GodSlayerEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //蓝色欧米茄魔石
-                if (item.type == Utility.FSDLC.Item("OmegaBlueEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("OmegaBlueEnchant" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("OmegaBlueEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("OmegaBlueEnchant" + ".Tail", cal));
-                    }
-
-                }
-
-                //瘟疫使者魔石
-                if (item.type == Utility.FSDLC.Item("PlaguebringerEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("PlaguebringerEnchant" + ".Main", cal));
-                }
-
-                //瘟疫死神魔石
-                if (item.type == Utility.FSDLC.Item("PlagueReaperEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("PlagueReaperEnchant" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("PlagueReaperEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("PlagueReaperEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //始源林海魔石
-                if (item.type == Utility.FSDLC.Item("SilvaEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("SilvaEnchant" + ".Main", cal));
-                    if (Utility.Calbard.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("SilvaEnchant" + ".Calbard", cal));
-                    }
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("SilvaEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("SilvaEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //斯塔提斯魔石
-                if (item.type == Utility.FSDLC.Item("StatigelEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("StatigelEnchant" + ".Main", cal));
-                    tooltips.Add(TooltipsPatch("StatigelEnchant" + ".Tail", cal));
-                }
-
-                //硫磺魔石
-                if (item.type == Utility.FSDLC.Item("SulphurousEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("SulphurousEnchant" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("SulphurousEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("SulphurousEnchant" + ".Tail", cal));
-                    }
-                }
-
-                //龙蒿魔石
-                if (item.type == Utility.FSDLC.Item("TarragonEnchant").Type)
-                {
-                    tooltips.Add(TooltipsPatch("TarragonEnchant" + ".Main", cal));
-                    if (Utility.Calbard.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("TarragonEnchant" + ".Calbard", cal));
-                    }
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch("TarragonEnchant" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch("TarragonEnchant" + ".Tail", cal));
-                    }
-                }
-                #endregion
-
-                #region 力和魂
-
                 string forces = "Forces."; //本地化文本专门分了灾厄的力和魂，这里要特殊处理一下
-
-                //湮灭之力
-                if (item.type == Utility.FSDLC.Item("AnnihilationForce").Type)
+                foreach (var target in CalEnchant) 
                 {
-                    tooltips.Add(TooltipsPatch(forces + "AnnihilationForce" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
+                    if (item.type == Utility.FSDLC.Item(target).Type) 
                     {
-                        tooltips.Add(TooltipsPatch(forces+"AnnihilationForce" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch(forces + "AnnihilationForce" + ".Tail", cal));
+                        var calbardText = TooltipsPatch(target + ".Calbard", cal);
+                        var tailText = TooltipsPatch(target + ".Tail", cal);
+                        var fargoText = TooltipsPatch(target + ".FargoCross", cal);
+
+                        tooltips.Add(TooltipsPatch(target + ".Main", cal));
+                        if (Utility.Calbard.Load && calbardText != null) 
+                        {
+                            tooltips.Add(calbardText);
+                        }
+                        if (Utility.FargoCross.Load && fargoText != null)
+                        {
+                            tooltips.Add(fargoText);
+                        }
+                        else if (tailText != null)
+                        {
+                            tooltips.Add(tailText);
+                        }
                     }
                 }
 
-                //荒芜之力
-                if (item.type == Utility.FSDLC.Item("DesolationForce").Type)
+                foreach (var target in CalForce) 
                 {
-                    tooltips.Add(TooltipsPatch(forces + "DesolationForce" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
+                    if (item.type == Utility.FSDLC.Item(target).Type)
                     {
-                        tooltips.Add(TooltipsPatch(forces + "DesolationForce" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch(forces + "DesolationForce" + ".Tail", cal));
-                    }
-                }
+                        var forceTailtext = TooltipsPatch(forces + target + ".Tail", cal);
+                        var forceFargotext = TooltipsPatch(forces + target + ".FargoCross", cal);
 
-                //毁灭之力
-                if (item.type == Utility.FSDLC.Item("DevastationForce").Type)
-                {
-                    tooltips.Add(TooltipsPatch(forces + "DevastationForce" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch(forces + "DevastationForce" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch(forces + "DevastationForce" + ".Tail", cal));
-                    }
-                }
-
-                //升华之力
-                if (item.type == Utility.FSDLC.Item("ExaltationForce").Type)
-                {
-                    tooltips.Add(TooltipsPatch(forces + "ExaltationForce" + ".Main", cal));
-                    if (Utility.FargoCross.Load)
-                    {
-                        tooltips.Add(TooltipsPatch(forces + "ExaltationForce" + ".FargoCross", cal));
-                    }
-                    else
-                    {
-                        tooltips.Add(TooltipsPatch(forces + "ExaltationForce" + ".Tail", cal));
+                        tooltips.Add(TooltipsPatch(forces + target + ".Main", cal));
+                        if (Utility.FargoCross.Load && forceFargotext != null)
+                        {
+                            tooltips.Add(forceFargotext);
+                        }
+                        else if (forceTailtext != null)
+                        {
+                            tooltips.Add(forceTailtext);
+                        }
                     }
                 }
 
@@ -391,9 +152,7 @@ namespace FargoCP.LocalizationPatch
                     }
                     tooltips.Add(TooltipsPatch(forces + "CalamitySoul" + ".Tail", cal));
                 }
-                #endregion
             }
-
 
             #endregion
 
